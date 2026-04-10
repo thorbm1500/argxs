3a77e6b15e94b732967a30cd15188b1924754fa4
Thorbm1500 <thorbm@proton.me>
2026-04-10 16:44:23 +0200

    Added sponsored message for VivaBit for providing hosting

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,39 @@
+<script>
+import VivaBitLogo from '$lib/assets/icons/VivaBitLogo.svelte';
+</script>
+
+<section class="footer-section">
+	<a href="https://vivabit.io">Powered by <VivaBitLogo/></a>
+</section>
+
+<style>
+	.footer-section {
+			display: flex;
+			flex-flow: row nowrap;
+			align-items: center;
+			justify-content: center;
+
+      width: 100%;
+
+			padding-top: 1rem;
+
+			font-family: 'Lexend', sans-serif;
+			font-size: .85rem;
+
+			user-select: none;
+
+			a {
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: center;
+          justify-content: center;
+					gap: .15rem;
+
+					color: var(--theme-text-secondary);
+			}
+
+			a:hover {
+					color: var(--theme-text-primary);
+			}
+	}
+</style>
