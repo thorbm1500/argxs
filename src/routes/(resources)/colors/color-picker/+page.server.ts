08fe482ae9fe75e59df5a3b1ec37ac03b7e0ad75
Thor B. <thorbm@outlook.dk>
2026-08-04 12:55:29 +0200

    Improved SEO

--- src/routes/(resources)/colors/color-picker/+page.server.ts
+++ src/routes/(resources)/colors/color-picker/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
-import type { SEO } from '$lib/components/interfaces';
+import { getDefaultSEODescription, type SEO } from '$lib/components/interfaces';
 
 export const ssr = true;
 
@@ -7,7 +7,8 @@ export const load: PageServerLoad = async ({ locals }) => {
 	return {
 		theme: locals.theme ?? 'dark',
 		seo: {
-			title: 'argxs ▪ '
+			title: 'argxs ▪ ',
+			description: getDefaultSEODescription()
 		} as SEO
 	};
 };
