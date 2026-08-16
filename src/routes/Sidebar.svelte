5681c4621a6dfc3373f5a3b14d6eb3473793ef1b
Thor B. <thorbm@outlook.dk>
2026-08-16 17:00:17 +0200

    Started work on new page: Core Concept

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -44,12 +44,12 @@
 				</svg>
 				<p class="text" style="--bg-x: {Math.floor(Math.random() * 500) + 1000}%">Home</p>
 			</a>
-			<div class={{ tag: true, in_progress: true }} tabindex="-1">
+			<a {@attach tabIndexFocus} class={{ page: true, newly_added: true, tag: true, selected: page.url.pathname === '/concept' }} href="/concept">
 				<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 					<path d="M13 2L4.09344 12.6879C3.74463 13.1064 3.57023 13.3157 3.56756 13.4925C3.56524 13.6461 3.63372 13.7923 3.75324 13.8889C3.89073 14 4.16316 14 4.70802 14H12L11 22L19.9065 11.3121C20.2553 10.8936 20.4297 10.6843 20.4324 10.5075C20.4347 10.3539 20.3663 10.2077 20.2467 10.1111C20.1092 10 19.8368 10 19.292 10H12L13 2Z" />
 				</svg>
 				<p class="text" style="--bg-x: {Math.floor(Math.random() * 500) + 1000}%">Core Concept</p>
-			</div>
+			</a>
 			<a {@attach tabIndexFocus} class={{ page: true, selected: page.url.pathname === '/privacy' }} href="/privacy">
 				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -107,7 +107,7 @@
 				</svg>
 				<p>COLORS</p>
 			</div>
-			<a {@attach tabIndexFocus} class={{ page: true, newly_added: true, tag: true, selected: page.url.pathname === '/colors/color-picker' }} href="/colors/color-picker">
+			<a {@attach tabIndexFocus} class={{ page: true, tag: true, selected: page.url.pathname === '/colors/color-picker' }} href="/colors/color-picker">
 				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 					<path d="M11 7l6 6" />
--- src/routes/concept/+page.svelte
+++ src/routes/concept/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	const { data } = $props();
 </script>
 
@@ -10,40 +10,185 @@
 	{/if}
 </svelte:head>
 
-<section class='concept-sec'>
-	<div class='title'>
+<section class="concept-sec">
+	<div class="top">
 		<h1>Core Concept</h1>
-		<svg width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round'>
-			<path d='M13 3l0 7l6 0l-8 11l0 -7l-6 0l8 -11'/>
-		</svg>
+		<h3>Last Updated</h3>
+	</div>
+	<div class="part intro">
+		<h1>The story behind argxs</h1>
+		<span>
+		<i>Insert story behind argxs</i>
+		</span>
+	</div>
+	<div class="part faq">
+		<h1>Frequently Asked Questions</h1>
+		<span>
+			Let's be completely honest. No one has asked these questions, or any questions actually.. So here are some questions and answers that I could imagine I would have.
+		</span>
+		<div class="question">
+			<h2>
+				Will argxs always be free?
+			</h2>
+			<h3>
+				Yes. argxs will always be both <strong>free</strong> and <strong>Open Source</strong>.
+			</h3>
+		</div>
+		<div class="question">
+			<h2>
+				Can I use *<i>insert resource here</i>* in my project/product?
+			</h2>
+			<h3>
+				All assets & resources provided by argxs are provided as is, and all rights are still held by the respective owners. argxs simply collects all in one place for ease of use. This means that if you wish to use a logo from fx. GitHub, you would have to contact GitHub to ask for permission.
+			</h3>
+		</div>
+		<div class="question">
+			<h2>
+				Does argxs earn any money?
+			</h2>
+			<h3>
+				No. argxs is run and developed solely by <a href="https://github.com/thorbm1500" rel="external" style="color:var(--theme-color-accent)"><i>me</i></a>, and no money is or has been made, <i>at all</i>.
+			</h3>
+		</div>
+		<div class="question">
+			<h2>
+				What pays for the servers?
+			</h2>
+			<h3>
+				argxs is lucky enough to have our servers sponsored by <a href="https://github.com/thorbm1500" rel="external" style="color:var(--theme-color-accent)">VivaBit</a>, allowing us to continue providing free and Open Source resources!
+			</h3>
+		</div>
+		<div class="question">
+			<h2>
+				Can I sponsor my <i>*Brand/Product/Logo/Etc*</i> on argxs?
+			</h2>
+			<h3>
+				No.
+			</h3>
+		</div>
 	</div>
 </section>
 
 <style>
-	.concept-sec {
-		.title {
-			display: flex;
-			flex-flow: row nowrap;
-			align-items: center;
-			justify-content: flex-start;
-			gap: .2rem;
-			
-			h1 {
-				font-size: 2.5rem;
-			}
-			
-			svg {
-				position: relative;
-				width: 3.5rem;
-				height: 3.5rem;
-				stroke-width: 2;
-				justify-self: flex-end;
-				
-				color: var(--theme-color-aware);
-				filter: drop-shadow(0 0 .25rem rgba(from var(--theme-color-aware) r g b / .25));
-				
-				transform: rotate(3deg);
-			}
-		}
-	}
+    /* Desktop & Tablet */
+    @media (width >= 44rem) {
+        .concept-sec {
+            padding-top: 4.5rem;
+
+            .top {
+                margin-bottom: 1.5rem;
+
+                h3 {
+                    font-size: .9rem;
+                }
+            }
+
+            .part {
+                margin-bottom: 2.25rem;
+
+                h1 {
+                    font-size: 1.8rem;
+                }
+
+                h2 {
+                    font-size: 1.2rem;
+                }
+
+                &.faq .question {
+                    margin-top: 1rem;
+                }
+            }
+        }
+    }
+
+    /* Phone */
+    @media (width < 44rem) {
+        .concept-sec {
+            padding-top: 1rem;
+
+            .top {
+                margin-bottom: 2.5rem;
+
+                h1 {
+                    margin-top: 1rem;
+                    transform: translateY(.75rem);
+                }
+
+                h3 {
+                    font-size: .8rem;
+                    margin-top: 0;
+                    transform: translateX(.7rem);
+                    opacity: .75;
+                }
+            }
+
+            .part {
+                margin-bottom: 2.5rem;
+                line-height: 1.25 !important;
+                letter-spacing: normal;
+
+                h1 {
+                    text-align: start;
+                    font-size: 1.75rem;
+                }
+
+                h2 {
+                    text-align: start;
+                    font-size: 1.2rem;
+                }
+
+                h3 {
+                    text-align: start;
+                    font-size: .95rem;
+                }
+
+                span {
+                    font-size: .85rem;
+                    font-weight: 600;
+                }
+
+                &.faq .question {
+                    margin-top: 1.5rem;
+
+										h3 {
+												margin-top: .125rem;
+										}
+                }
+            }
+        }
+    }
+
+    .concept-sec {
+        font-family: 'Funnel Sans', sans-serif;
+        color: var(--theme-text-primary);
+        font-weight: 500;
+
+        span {
+            font-family: 'Google Sans', 'Funnel Sans', sans-serif;
+        }
+
+        .top {
+            h1 {
+                font-size: 3.25rem;
+                font-weight: 850;
+                user-select: none;
+            }
+
+            h3 {
+                font-weight: 800;
+                color: var(--theme-text-third);
+                align-self: start;
+            }
+        }
+
+        .part {
+            color: var(--theme-text-primary);
+            text-align: justify;
+
+            h2 {
+                font-weight: 750;
+                user-select: none;
+            }
+        }
+    }
 </style>
