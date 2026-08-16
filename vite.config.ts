612de1b48e584e147c9808c15e6471a9e5438bef
Thor B. <thorbm@outlook.dk>
2026-08-16 18:54:01 +0200

    Bump to version 0.9.0

--- vite.config.ts
+++ vite.config.ts
@@ -63,7 +63,7 @@ export default defineConfig({
         trustedOrigins: ["https://argxs.dev", "https://dev.argxs.dev"],
       },
       version: {
-        name: "0.8.1",
+        name: "0.9.0",
         pollInterval: 120000,
       },
     }),
