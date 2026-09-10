3999ef48cf647d44e1574d87f35951c72e2f2216
Thor B. <thorbm@outlook.dk>
2026-09-10 11:43:35 +0200

    Bump to v0.9.2

---
+++ src/lib/server/resources/changelogs/0.9.2/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Version 0.9.2
+#### 2026-xx-xx
+
+## Minor Changes
+- **Empty**
+
+## Bug Fixes
+- **Empty**
+
+## Dependencies
+- **Empty**
--- vite.config.ts
+++ vite.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
         trustedOrigins: ["https://argxs.dev", "https://dev.argxs.dev"],
       },
       version: {
-        name: "0.9.1",
+        name: "0.9.2",
         pollInterval: 120000,
       },
     }),
